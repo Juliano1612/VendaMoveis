e19cf656f9a08ba23e4fd0c1e750031a947d5c37
AndersonOyama <anderson.oyama@hotmail.com>
2016-10-07 19:26:17 -0300

    Hibernate implementado
    Hibernate implementado e tela do gerente refeito devido a bug

--- src/Controle/ImagemProduto.java
+++ src/Controle/ImagemProduto.java
@@ -1,5 +1,5 @@
 package Controle;
-// Generated 30/09/2016 15:49:28 by Hibernate Tools 4.3.1
+// Generated 07/10/2016 17:26:32 by Hibernate Tools 4.3.1
 
 
 
---
+++ src/TelaGerente/FXMLTelaGerenteController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TelaGerente;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author ander
+ */
+
+
+public class FXMLTelaGerenteController implements Initializable {
+
+    @FXML
+    private Button buttonFecharTelaGerente;
+    
+    @FXML
+    void buttonFecharTelaGerenteAction(ActionEvent event){
+        Platform.exit();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }   
+}
